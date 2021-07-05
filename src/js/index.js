6e685b07442367f6e4b42692e98ad0a32b7869df
Galyna Khyzhynska <gal4enock86@gmail.com>
2021-07-05 20:51:04 +0300

    добавляет мягкую прокрутку

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,30 @@ const refs = {
 
 window.addEventListener('scroll', function() {
   refs.header.classList.add("newHeader")
-  console.log(pageYOffset);
-  // console.log(window.innerWidth);
-  if (window.innerWidth >= 320 && pageYOffset > 120
+ if (window.innerWidth >= 320 && pageYOffset > 120
     || window.innerWidth >= 768 && pageYOffset > 70
-  || window.innerWidth >= 1360 && pageYOffset > 70) {
+    || window.innerWidth >= 1360 && pageYOffset > 70) {
      refs.header.classList.add("headerScrolled")
   } else refs.header.classList.remove("headerScrolled")
+});
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.scrollto').offsetHeight;
+        // const topOffset = 0; // если не нужен отступ сверху 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
 });
