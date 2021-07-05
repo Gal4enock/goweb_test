2f969e349d29b27af52c4290301e50f8af3ae2e2
Galyna Khyzhynska <gal4enock86@gmail.com>
2021-07-05 22:05:21 +0300

    добавляет библиотеку lightbox

--- src/js/index.js
+++ src/js/index.js
@@ -38,6 +38,5 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
 refs.button.addEventListener("click", () => {
   if (refs.input.value.length < 5) {
     refs.input.insertAdjacentHTML("afterend", '<div class="warning-icon"><div/>')
-    console.log(refs.input.value.length);
   }
 })
